2ea3af13be5a8a619a39c29dc8345cdcf9ec2860
Pam Cairo <pcairo_90@outlook.com>
2020-02-15 18:52:01 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,4 @@
-  class Person
+class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
                 :weight, :handed, :complexion, :t_shirt_size,
                 :wrist_size, :glove_size, :pant_length, :pant_width
@@ -8,5 +8,4 @@
       self.send(("#{key}="), value)
     end
   end
-end
 end
